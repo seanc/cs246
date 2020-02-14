481dc68fd703cddb1fa15a49b511334923f45715
Sean <hello@imsean.me>
2020-02-14 11:23:06 -0500

    add swap counts

---
+++ lab6/lab06.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "Array.h"
+
+/**********************************
+ * Question 1: Swap Counts
+ * a. 4
+ * b. 6
+ * c. 6 - does each swap that occurs in the innermost iteration count as an individual swap or a all swaps to its proper place count as one swap?
+ **********************************/
+
+template <typename T>
+bool Monotonic(const Array<T>& data)
+{
+    return false;
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    return 0; 
+}
