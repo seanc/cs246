6fe5970cae4ffbc1fb850d93b92d44fee881b795
Sean <hello@imsean.me>
2020-03-10 22:52:10 -0400

    add union

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -24,9 +24,14 @@ ds::sn::Set<T> Intersection(const ds::sn::Set<T>& A,const ds::sn::Set<T>& B)
 template <typename T>
 ds::sn::Set<T> Union(const ds::sn::Set<T>& A,const ds::sn::Set<T>& B)
 {
-    ds::sn::Set<T>* tmp;
+    ds::sn::Iterator<T>* iterB = B.ToIterator();
+    ds::sn::Set<T> C = A;
+    while (iterB->HasNext()) {
+        T bVal = iterB->Next();
+        C.Insert(bVal);
+    }
     
-    return ds::sn::Set<T>();
+    return C;
 }
 
 /********************************
@@ -54,11 +59,18 @@ int main()
 {
     ds::sn::Set<int> tree = GenerateSet(5);
     ds::sn::Set<int> teaTree = GenerateSet(5);
+
+    std::cout << "A: " << tree << std::endl;
+    std::cout << "B: " << teaTree << std::endl;
+
+    std::cout << "--- INTERSECTION ---" << std::endl;
     
-    std::cout << tree << std::endl;
-    std::cout << teaTree << std::endl;
-    std::cout << Intersection(tree, teaTree) << std::endl;
-    std::cout << Intersection(tree, tree) << std::endl;
+    std::cout << "A: " << Intersection(tree, teaTree) << std::endl;
+    std::cout << "B: " << Intersection(tree, tree) << std::endl;
+
+    std::cout << "--- UNION ---" << std::endl;
+    std::cout << "A: " << Union(tree, teaTree) << std::endl;
+    std::cout << "B: " << Union(teaTree, tree) << std::endl; 
 
     return 0; 
 }
