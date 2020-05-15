442beab939aaca6ad272a5be4adad1ff00a19459
Sean <hello@imsean.me>
2020-05-15 00:03:01 -0400

    complete lab8

--- lab8/lab08.cpp
+++ lab8/lab08.cpp
@@ -15,13 +15,42 @@
  * Know the previous nodes to the nodes that you want to swap
  * 
  **/
-// template <typename T>
-// void SwapNodes(ds::Node<T>* root,ds::Node<T>* nptr1,ds::Node<T>* nptr2)
-// {
-//     ds::Node<int>* tmp = nptr1;
+template <typename T>
+void SwapNodes(ds::Node<T>* root,ds::Node<T>* nptr1,ds::Node<T>* nptr2)
+{
+    ds::Node<T>* prev1, *prev2;
+    ds::Node<T>* tmp1 = root, *tmp2 = root;
 
-//     return;
-// }
+    while (tmp1 != NULL && tmp1->GetData() != nptr1->GetData()) {
+        prev1 = tmp1;
+        prev1 = prev1->GetLink();
+    }
+
+    while (tmp2 != NULL && tmp2->GetData() != nptr2->GetData()) {
+        prev2 = tmp2;
+        prev2 = prev2->GetLink();
+    }
+
+    if (prev1 == NULL || prev2 == NULL) {
+        return;
+    }
+
+    if (prev1 != NULL) {
+        prev1->SetLink(tmp2);
+    } else {
+        root = tmp2;
+    }
+
+    if (prev2 != NULL) {
+        prev2->SetLink(tmp1);
+    } else {
+        root = tmp1;
+    }
+
+    ds::Node<T>* tmp = tmp2->GetLink();
+    tmp2->SetLink(tmp1->GetLink());
+    tmp1->SetLink(tmp);
+}
 
 // double SecondLargest(ds::Node<double>* root)
 // {
@@ -32,39 +61,38 @@ template <typename T>
 void RemoveAll(ds::Node<T>*& root, const T& target) {
     if (root == NULL) return;
 
-    if (root->GetData() == target) {
-        ds::Node<T> *remove_ptr = root;
-        root = root->GetLink();
-        delete remove_ptr;
-        remove_ptr = NULL;
-    }
+    // if (root->GetData() == target) {
+    //     ds::Node<T> *remove_ptr = root;
+    //     root = root->GetLink();
+    //     delete remove_ptr;
+    //     remove_ptr = NULL;
+    // }
 
     ds::Node<T> *tmp = root;
-    ds::Node<T> *prev = root;
+    ds::Node<T> *prev;
+
+    while (tmp != NULL && tmp->GetData() == target) {
+        root = tmp->GetLink();
+        delete tmp;
+        tmp = root;
+    }
 
     while (tmp != NULL) {
         if (tmp->GetData() == target) {
-            if (prev != NULL) {
-                prev->SetLink(tmp->GetLink());
-            } else {
-                prev = tmp->GetLink();
-                tmp = prev->GetLink();
+            while (tmp != NULL && tmp->GetData() == target) {
+                ds::Node<T>* tmp2 = tmp->GetLink();
+                delete tmp;
+                tmp = tmp2;
             }
-        } else {
+
+            prev->SetLink(tmp);
+        }
+
+        if (tmp != NULL) {
             prev = tmp;
             tmp = tmp->GetLink();
         }
     }
-
-    // while (tmp->GetLink() != NULL) {
-    //     if (tmp->GetLink()->GetData() == target) {
-    //         tmp->SetLink(tmp->GetLink()->GetLink());
-    //     } else {
-    //         tmp = tmp->GetLink();
-    //     }
-    // }
-
-    return;
 }
 
 template <class T>
